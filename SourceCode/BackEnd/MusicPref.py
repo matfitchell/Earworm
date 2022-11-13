3905666c7f39f060c9a902155ce79d33e77bc5f2
matfitchell <64942086+matfitchell@users.noreply.github.com>
2022-11-13 00:35:27 -0800

    updated contents slightly

---
+++ SourceCode/BackEnd/MusicPref.py
@@ -0,0 +1,9 @@
+#ML implementation For Music Preference detection
+
+#import libraries for spotify analytics and jupyter
+
+
+#import web scraping for top 100 charts \
+
+
+#import CSV to feed to CSV 
