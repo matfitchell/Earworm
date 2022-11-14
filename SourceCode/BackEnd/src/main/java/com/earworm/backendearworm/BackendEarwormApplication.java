8a517e5410f1797de680e2bbfeb93cefec418cd7
matfitchell <64942086+matfitchell@users.noreply.github.com>
2022-11-14 02:07:29 -0800

    Implementing spring boot

---
+++ SourceCode/BackEnd/src/main/java/com/earworm/backendearworm/BackendEarwormApplication.java
@@ -0,0 +1,13 @@
+package com.earworm.backendearworm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEarwormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEarwormApplication.class, args);
+	}
+
+}
