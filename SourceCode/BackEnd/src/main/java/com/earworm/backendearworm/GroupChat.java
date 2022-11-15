a0de08246b138b4ec9e95da6c681f7191620674a
matfitchell <64942086+matfitchell@users.noreply.github.com>
2022-11-15 15:45:02 -0800

    shitty update

--- SourceCode/BackEnd/src/main/java/com/earworm/backendearworm/GroupChat.java
+++ SourceCode/BackEnd/src/main/java/com/earworm/backendearworm/GroupChat.java
@@ -1,5 +1,23 @@
 package com.earworm.backendearworm;
 
 public class GroupChat {
+
+    public class SentMessage{
+
+        public class MessageContents {
+            //This sent message object
+            SentMessage message;
+
+            //What was sent in the message
+            String contents;
+
+            public String getAsHTML(){
+                return
+                //details of message
+            }
+
+        }
+
+    }
     
 }
