5001fe47022920707cd89219eecc87951921060a
Rodolfo Rivera <rorivera0621@gmail.com>
2022-11-16 19:00:29 -0800

    User.java

--- SourceCode/BackEnd/src/main/java/com/earworm/backendearworm/User.java
+++ SourceCode/BackEnd/src/main/java/com/earworm/backendearworm/User.java
@@ -1,11 +1,24 @@
 package com.earworm.backendearworm;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import javax.persistence.Entity;
+
 @Entity
-public class User {
+public class User implements UserDetails{
 
     private String displayName = " ", username = " ", email = " ", password = " ", bio = " ";
     private int age = 0, zipCode = 0, phone = 0;
-    public User(){
-
+    
+    //Class Constructer
+    public User(String displayName, String username, String email, String password, int age, int zipCode, int phone) {
+        this.displayName = displayName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.age = age;
+        this.zipCode = zipCode;
+        this.phone = phone;
     }
+    
 }
