c18de5c1280c15e6c8e972228b2422c0e568d287
Rodolfo Rivera <rorivera0621@gmail.com>
2022-11-16 17:39:41 -0800

    User

--- SourceCode/BackEnd/src/main/java/com/earworm/backendearworm/User.java
+++ SourceCode/BackEnd/src/main/java/com/earworm/backendearworm/User.java
@@ -1,4 +1,11 @@
 package com.earworm.backendearworm;
+
+@Entity
 public class User {
-    
+
+    private String displayName = " ", username = " ", email = " ", password = " ", bio = " ";
+    private int age = 0, zipCode = 0, phone = 0;
+    public User(){
+
+    }
 }
