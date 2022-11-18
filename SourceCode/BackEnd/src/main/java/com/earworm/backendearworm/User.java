b9eeb36646c5e156c7bfbfb405f3b8576e9f9438
Rodolfo Rivera <rorivera0621@gmail.com>
2022-11-18 13:18:20 -0800

    User get set

--- SourceCode/BackEnd/src/main/java/com/earworm/backendearworm/User.java
+++ SourceCode/BackEnd/src/main/java/com/earworm/backendearworm/User.java
@@ -7,18 +7,96 @@ import javax.persistence.Entity;
 @Entity
 public class User implements UserDetails{
 
-    private String displayName = " ", username = " ", email = " ", password = " ", bio = " ";
-    private int age = 0, zipCode = 0, phone = 0;
+    private String displayName = " ";
+    private String username = " ";
+    private String email = " ";
+    private String password = " ";
+    private String bio = " ";
+    private int age = 0;
+    private int zipCode = 0;
+    private int phone = 0;
     
     //Class Constructer
     public User(String displayName, String username, String email, String password, int age, int zipCode, int phone) {
-        this.displayName = displayName;
+        this.setDisplayName(displayName);
+        this.setUsername(username);
+        this.setEmail(email);
+        this.setPassword(password);
+        this.setAge(age);
+        this.setZipCode(zipCode);
+        this.setPhone(phone);
+    }
+
+    //Default Constructer
+    public User{
+    }
+
+    public User(String username, String email, String password){
         this.username = username;
         this.email = email;
         this.password = password;
+    }
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
         this.zipCode = zipCode;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
         this.phone = phone;
     }
-    
 }
