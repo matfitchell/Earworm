1ce31355e06a7d200e031a3bd8f44b752249e669
matfitchell <64942086+matfitchell@users.noreply.github.com>
2022-11-21 17:24:12 -0800

    Added more dependancies to pom.xml

--- SourceCode/BackEnd/src/main/java/com/earworm/backendearworm/BackdendEarwormApplication.java
+++ SourceCode/BackEnd/src/main/java/com/earworm/backendearworm/BackdendEarwormApplication.java
@@ -2,6 +2,7 @@ package com.earworm.backendearworm;
 
 import java.sql.*;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
