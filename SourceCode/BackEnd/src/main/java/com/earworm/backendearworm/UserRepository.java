25235804b8198bd546d0d2f4f2efcc03d89b41d6
matfitchell <64942086+matfitchell@users.noreply.github.com>
2022-12-13 00:11:56 -0800

    Update push

--- SourceCode/BackEnd/src/main/java/com/earworm/backendearworm/UserRepository.java
+++ SourceCode/BackEnd/src/main/java/com/earworm/backendearworm/UserRepository.java
@@ -1,8 +1,10 @@
 package com.earworm.backendearworm;
 
+
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,7 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query("SELECT s FROM User s WHERE s.email = ?1")
+   /*  @Query("SELECT s FROM User s WHERE s.email = ?1")
     Optional<User> findUserByEmail(String email);
 
     List<User> findAllByZipcode(int zipCode);
@@ -24,4 +26,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Modifying
     @Query("UPDATE user a " + "Set a.enabled = TRUE WHERE a.email = ?1")
     int enableUser(String email);
+
+*/
+    
 }
