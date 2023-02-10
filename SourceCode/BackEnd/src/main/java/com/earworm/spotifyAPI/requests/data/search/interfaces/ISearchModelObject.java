d89c2ceef41cf0c35e8fd1c742c39f319260106d
matfitchell <64942086+matfitchell@users.noreply.github.com>
2023-02-09 19:38:05 -0800

    Removed Source Code

--- SourceCode/BackEnd/src/main/java/com/earworm/spotifyAPI/requests/data/search/interfaces/ISearchModelObject.java
+++
@@ -1,7 +0,0 @@
-package com.earworm.spotifyAPI.requests.data.search.interfaces;
-
-import com.earworm.spotifyAPI.model_objects.IModelObject;
-
-public interface ISearchModelObject extends IModelObject {
-
-}
