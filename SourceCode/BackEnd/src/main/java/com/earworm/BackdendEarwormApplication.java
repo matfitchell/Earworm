c46119c38e3359ef9c7cf4aa1ee850d6657f92d0
Rodolforiv0621 <78060969+Rodolforiv0621@users.noreply.github.com>
2023-02-10 17:41:43 -0800

    Fixed backend, deleted gcloud connection files

--- SourceCode/BackEnd/src/main/java/com/earworm/BackdendEarwormApplication.java
+++ SourceCode/BackEnd/src/main/java/com/earworm/BackdendEarwormApplication.java
@@ -10,36 +10,24 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 //import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.util.backoff.BackOffExecution;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLException;
 
-import com.earworm.backendearworm.GCloudConnector;
-
 @SpringBootApplication
 @RestController
-
 public class BackdendEarwormApplication {
 
 	public static void main(String[] args) {
 
-		// SpringBoot Launch
 		SpringApplication.run(BackdendEarwormApplication.class, args);
+	}
 
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-			@Override
-			public void run() {
-				try {
-					Connection conn = GCloudConnector.getInstance().connection;
-					if (conn != null) {
-						conn.close();
-						System.out.println("Database Connection Terminated");
-					}
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-		});
+	@GetMapping("/")
+	String hello() {
+		return "Hello";
 	}
 }
