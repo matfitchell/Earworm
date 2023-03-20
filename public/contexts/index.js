2ef3edcbe942183bb30a2994d60dc43c65746a59
Nat <100656878+nct31@users.noreply.github.com>
2023-03-19 17:08:40 -0700

    :(

---
+++ public/contexts/index.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './pages/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+{/*-----use state for login to sign up idk-----*/}
+{/*const [currentForm, setCurrentForm] = useState ('login');*/}
+
+root.render(
+    <App />
+);
+
+
