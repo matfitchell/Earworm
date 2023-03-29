c188d9dd6c9839f7ebb2f0bd54db6a6a915fcbf6
Mitch <64942086+matfitchell@users.noreply.github.com>
2023-03-28 20:52:28 -0700

    Update kmeanscluster.py

--- SourceCode/ML/SpotifyJsonPuler/kmeanscluster.py
+++ SourceCode/ML/SpotifyJsonPuler/kmeanscluster.py
@@ -1,19 +1,28 @@
-import numpy as np
+import pandas as pd 
+import numpy as np 
 import matplotlib.pyplot as plt
-import pandas as pd
-import random
 
 
-dataset = pd.read_csv("featuresFile_buttercheekman.csv")
+df = pd.read_csv("featuresFile_buttercheekman.csv", encoding = 'utf8')
 
-x = dataset
+i = 0
 
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-ct = ColumnTransformer(transformers= [("encoder", OneHotEncoder(), [-14,-13,-12])])
-x_train = np.array(ct.fit_transform(x))
- 
-from sklearn.cluster import KMeans
-km = KMeans(n_clusters = 5, init = "k-means++", random_state=5)
-y_means = km.fit(x_train)
+# Defining Dependent and Independant variable
+X = np.array(df[['danceability ', 'energy ', 'key ', 'acousticness ', 'liveness ', 'valence ' , 'duration_ms ']])
+Y = df['tempo   '].values
 
+# Plotting the Clusters using matplotlib
+plt.rcParams['figure.figsize'] = [14, 7]
+plt.rc('font', size=14)
+
+plt.scatter(df['duration_ms '], df['danceability '], label="Dancability")
+plt.scatter(df['duration_ms '], df['energy '], label="Energy")
+#plt.scatter(df['duration_ms '], df['key '], label="key")
+plt.scatter(df['duration_ms '], df['acousticness '], label="Acousticness")
+plt.scatter(df['duration_ms '], df['liveness '], label="Liveness")
+plt.scatter(df['duration_ms '], df['valence '], label="Valence")
+
+plt.xlabel("Duration")
+plt.ylabel("Features")
+plt.legend()
+plt.show()
