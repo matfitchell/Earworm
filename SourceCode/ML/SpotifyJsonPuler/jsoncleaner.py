2f75f508c5d3104e170da366f6e9eabec0f1ce99
matfitchell <64942086+matfitchell@users.noreply.github.com>
2023-03-30 16:30:43 -0700

    Update jsoncleaner.py
    Need to change X, and Y variables for appropriate features. Base code for linear regression

--- SourceCode/ML/SpotifyJsonPuler/jsoncleaner.py
+++ SourceCode/ML/SpotifyJsonPuler/jsoncleaner.py
@@ -107,6 +107,19 @@ for i in range(0,len(features_df['id'])):
 
 
 
+data = pd.read_csv("featuresFile_' + username + '.csv")
+X = data.iloc[:,0].values.reshape(-1,1)
+Y = data.iloc[:,1].values.reshape(-1,1)
+linear_regressor = LinearRegression()
+linear_regressor.fit(X, Y)
+Y_pred = linear_regressor.predict(X)
+
+plt.scatter(X, Y)
+plt.plot (X, Y_pred, color='red')
+plt.show()
+
+
+
 # descending_order = all_songs['artist'].value_counts().sort_values(ascending=False).index
 # ax = sb.countplot(y = all_songs['artist'], order=descending_order)
 
