d0bf7b252838e7f3749f2a7f998e1fbba9f6bcd3
Mitch <64942086+matfitchell@users.noreply.github.com>
2023-04-13 20:24:43 -0700

    Cleaned up the Directory

--- SourceCode/BackEnd/src/main/java/com/earworm/backendearworm/MusicPref.py
+++
@@ -1,9 +0,0 @@
-#ML implementation For Music Preference detection
-
-#import libraries for spotify analytics and jupyter
-
-
-#import web scraping for top 100 charts \
-
-
-#import CSV to feed to CSV 
--- SourceCode/BackEnd/src/main/java/com/earworm/email/EmailSender.java
+++
@@ -1,6 +0,0 @@
-package com.earworm.email;
-
-public interface EmailSender {
-    void send(String to, String email);
-
-}
