14a762bc08b9f6f32c97d50371ac83f53f314f90
matfitchell <64942086+matfitchell@users.noreply.github.com>
2023-04-20 16:18:44 -0700

    Update App.js
    started to add spotify api integration

--- src/App.js
+++ src/App.js
@@ -46,6 +46,68 @@ function App() {
     return <redirect to = "./Homepage"/>;
   }
 
+  //spotify api integration
+
+  const APIController = (function() {
+
+
+        const clientID = "2100da3530bc4465b471b768a7309a4a";
+        const clientSecret = "cf9725e022e94dcd98a49f4445b7b585";
+
+        const_getToken = async () => {
+
+          const result = await fetch('https://accounts.spotify.com/api/token', {
+            method : "POST",
+            headers: {
+              'Content-Type' : 'application/x-www-form-urlencoded',
+              'Authorization' : 'Basic' + btoa(clientID + clientSecret)
+            },
+            body: 'grant_type=client_credentials'
+          });
+          
+          const data = await result.json();
+          return data.access_token;
+
+        }
+
+        const _getGenres = async (token) => {
+
+            const result = await fetch ('https://api.spotify.com/v1/browse/categories?locale=sv+US' , {
+              method : "GET",
+              headers : {'Authorization' : 'Bearer ' + token}
+            });
+
+            const data = await result.json();
+            return data.categories.items;
+        }
+
+        const _getPlaylistByGenre = async (token, genreId) => {
+
+          const limit = 10;
+
+          const result = await fetch('https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}', {
+          method: 'GET',
+          headers: {'Authorization' : "Bearer " + token}
+
+        });
+         const data = await result.json();
+         return data.playlists.items;
+        } 
+
+
+        const _getTracks = async (token, tracksEndPoint) => {
+
+          const limit = 10;
+
+          const result = await fetch('${tracksEndPoint}?limit=${limit}', {
+            method: 'GET',
+            headers: {'Authorization' : "Bearer " + token}
+
+          });
+        }
+
+      })
+
   return ( 
     <div className='homeContainer'>   {/*-----Home Container-----*/}
       <div className='flexSide left'> {/*-----left side-----*/}
