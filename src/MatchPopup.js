de7bd15caf5aff7bc3e0b9ed135dc8f9d5710b2d
Rodolforiv0621 <78060969+Rodolforiv0621@users.noreply.github.com>
2023-05-02 12:14:59 -0700

    added matchpopup js

---
+++ src/MatchPopup.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./MatchPopup.css";
+
+function MatchPopup(props) {
+  return props.trigger ? (
+    <div className="popup">
+      <div className="popup-inner">
+        <h2>You have matched with {props.firstName}!</h2>
+        {props.children}
+
+        <button className="message-btn">Message</button>
+        <button className="close-btn" onClick={() => props.setTrigger(false)}>
+          Continue Matching
+        </button>
+      </div>
+    </div>
+  ) : (
+    ""
+  );
+}
+export default MatchPopup;
